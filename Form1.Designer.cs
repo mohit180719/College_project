9463e95866a5c4ba0546dbdbe18482bfd3fa749e
mohit180719 <44685092+mohit180719@users.noreply.github.com>
2018-11-02 03:16:33 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace XML_TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ISA_VendorName = new System.Windows.Forms.TextBox();
+            this.ISA_ID = new System.Windows.Forms.TextBox();
+            this.ISA_Version = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ISA_Time = new System.Windows.Forms.TextBox();
+            this.ISA_Flag = new System.Windows.Forms.TextBox();
+            this.ISA_Date = new System.Windows.Forms.TextBox();
+            this.ISA_Button = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ISA_Button);
+            this.groupBox1.Controls.Add(this.ISA_Date);
+            this.groupBox1.Controls.Add(this.ISA_Flag);
+            this.groupBox1.Controls.Add(this.ISA_Time);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.ISA_Version);
+            this.groupBox1.Controls.Add(this.ISA_ID);
+            this.groupBox1.Controls.Add(this.ISA_VendorName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(574, 146);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ISA Segment";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vendor Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Version";
+            // 
+            // ISA_VendorName
+            // 
+            this.ISA_VendorName.Location = new System.Drawing.Point(108, 28);
+            this.ISA_VendorName.Name = "ISA_VendorName";
+            this.ISA_VendorName.Size = new System.Drawing.Size(100, 20);
+            this.ISA_VendorName.TabIndex = 3;
+            // 
+            // ISA_ID
+            // 
+            this.ISA_ID.Location = new System.Drawing.Point(108, 60);
+            this.ISA_ID.Name = "ISA_ID";
+            this.ISA_ID.Size = new System.Drawing.Size(100, 20);
+            this.ISA_ID.TabIndex = 4;
+            // 
+            // ISA_Version
+            // 
+            this.ISA_Version.Location = new System.Drawing.Point(108, 100);
+            this.ISA_Version.Name = "ISA_Version";
+            this.ISA_Version.Size = new System.Drawing.Size(100, 20);
+            this.ISA_Version.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(250, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Time";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(250, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Flag";
+            // 
+            // ISA_Time
+            // 
+            this.ISA_Time.Location = new System.Drawing.Point(309, 64);
+            this.ISA_Time.Name = "ISA_Time";
+            this.ISA_Time.Size = new System.Drawing.Size(100, 20);
+            this.ISA_Time.TabIndex = 9;
+            // 
+            // ISA_Flag
+            // 
+            this.ISA_Flag.Location = new System.Drawing.Point(309, 100);
+            this.ISA_Flag.Name = "ISA_Flag";
+            this.ISA_Flag.Size = new System.Drawing.Size(100, 20);
+            this.ISA_Flag.TabIndex = 10;
+            // 
+            // ISA_Date
+            // 
+            this.ISA_Date.Location = new System.Drawing.Point(309, 31);
+            this.ISA_Date.Name = "ISA_Date";
+            this.ISA_Date.Size = new System.Drawing.Size(100, 20);
+            this.ISA_Date.TabIndex = 11;
+            // 
+            // ISA_Button
+            // 
+            this.ISA_Button.Location = new System.Drawing.Point(434, 64);
+            this.ISA_Button.Name = "ISA_Button";
+            this.ISA_Button.Size = new System.Drawing.Size(134, 23);
+            this.ISA_Button.TabIndex = 12;
+            this.ISA_Button.Text = "Generate XML";
+            this.ISA_Button.UseVisualStyleBackColor = true;
+            this.ISA_Button.Click += new System.EventHandler(this.ISA_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 329);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ISA_Button;
+        private System.Windows.Forms.TextBox ISA_Date;
+        private System.Windows.Forms.TextBox ISA_Flag;
+        private System.Windows.Forms.TextBox ISA_Time;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ISA_Version;
+        private System.Windows.Forms.TextBox ISA_ID;
+        private System.Windows.Forms.TextBox ISA_VendorName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XML_TEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ISA_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XmlWriter xmlwriter = XmlWriter.Create("Mohit.xml");
+                xmlwriter.WriteStartDocument();
+
+                // ISA Segment Part
+
+                xmlwriter.WriteStartElement("EDI");
+                xmlwriter.WriteStartElement("ISA");
+                
+                xmlwriter.WriteStartElement("Vendor");
+                xmlwriter.WriteString(ISA_VendorName.Text);
+                xmlwriter.WriteEndElement();
+
+                xmlwriter.WriteStartElement("ID");
+                xmlwriter.WriteString(ISA_ID.Text);
+                xmlwriter.WriteEndElement();
+
+                xmlwriter.WriteStartElement("VendorName");
+                xmlwriter.WriteString(ISA_VendorName.Text);
+                xmlwriter.WriteEndElement();
+
+                xmlwriter.WriteStartElement("Date");
+                xmlwriter.WriteString(ISA_Date.Text);
+                xmlwriter.WriteEndElement();
+
+                xmlwriter.WriteStartElement("Time");
+                xmlwriter.WriteString(ISA_Time.Text);
+                xmlwriter.WriteEndElement();
+
+                xmlwriter.WriteStartElement("Flag");
+                xmlwriter.WriteString(ISA_Flag.Text);
+                xmlwriter.WriteEndElement();
+
+                xmlwriter.WriteEndDocument();
+                xmlwriter.Close();
+                MessageBox.Show("xmlwriter is Generated successfully");
+
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+                Console.Read();
+
+            }
+        }
+    }
+}
